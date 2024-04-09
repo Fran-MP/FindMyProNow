f96cefcc660298cc05f456802bbf7597c2b4d654
=Francisco Pereira <=franjmpereira@gmail.com>
2024-04-09 20:53:53 +0200

    Primer commit

---
+++ ktor_findmypronow/src/main/kotlin/com/findmypronow/Application.kt
@@ -0,0 +1,18 @@
+package com.findmypronow
+
+import com.findmypronow.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSockets()
+    configureSerialization()
+    configureSecurity()
+    configureRouting()
+}
---
+++ ktor_findmypronow/src/main/kotlin/com/findmypronow/models/User.kt
@@ -0,0 +1,11 @@
+package com.findmypronow.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(val id: String,
+                     val firstName: String,
+                     val lastName: String,
+                     val email: String,
+                     val password: String,
+                     val type: String)
